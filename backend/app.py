75aa696b804f6f3a754617524928e8a3753c8869
Matthew Franchini <168607935+matchewf11@users.noreply.github.com>
2025-05-17 12:02:25 -0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -3,8 +3,6 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-#heres our API key  for langchain: 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
-
 @app.route('/api/planner', methods=['POST'])
 def make_table():
     data = request.get_json()
