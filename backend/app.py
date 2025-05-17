e23d7aecb690cbfbe524890dcd27bf0f7ad98908
Matthew Franchini <matchewf11@gmail.com>
2025-05-17 11:59:22 -0700

    merged thing

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+#heres our API key  for langchain: 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
 
 @app.route('/api/planner', methods=['POST'])
 def make_table():
