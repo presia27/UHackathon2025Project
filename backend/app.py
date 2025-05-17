067a2318b364fa8bda89000c16fcc2209775c5f4
Matthew Franchini <matchewf11@gmail.com>
2025-05-17 11:56:28 -0700

    Set up brief backend send json 'task': 'sometihg'

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,14 @@
-#Hello from this
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/api/planner', methods=['POST'])
+def make_table():
+    data = request.get_json()
+    return jsonify({ "recieved": data })
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
